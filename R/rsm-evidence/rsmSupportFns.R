7a81dfb981a7c247941ae0aa63ee2ddc2291a7e8
dpc10ster <dpc10ster@gmail.com>
2020-12-24 16:49:11 -0500

    Almost finished with Swets observations
    Interesting; even if no body else will read this

--- R/rsm-evidence/rsmSupportFns.R
+++ R/rsm-evidence/rsmSupportFns.R
@@ -1,29 +1,10 @@
-erf <- function(x){
-  return (2 * pnorm(sqrt(2) * x) - 1)
-}
-
-xROC <- function(zeta, lambdaP){
-  # returns FPF, the abscissa of ROC curve
-  return( 1 - exp( (-lambdaP / 2) + 0.5 * lambdaP * erf(zeta / sqrt(2))))
-}
-
-yROC <- function(zeta, mu, lambdaP, nuP, lesionDistribution){
-  # returns TPF, the ordinate of ROC curve
-  fl <- lesionDistribution[, 2] / sum(lesionDistribution[, 2])
-  TPF <- 0
-  for (i in 1:nrow(lesionDistribution)){
-    TPF <- TPF + fl[i] * (1 - (1 - nuP/2 + nuP/2  *erf( (zeta - mu) / sqrt(2) ))^lesionDistribution[i, 1] * exp( (-lambdaP / 2) + 0.5 * lambdaP * erf(zeta / sqrt(2))))
-  }
-  return (TPF)
-}
-
-intROC <- function(FPF, mu, lambdaP, nuP, lesionDistribution){
+intROC <- function(FPF, mu, lambdaP, nuP, lesDistr){
   # returns TPF, the ordinate of ROC curve; takes FPF as the variable.
-  # AUC is calculated by integrating this function in terms of FPF
+  # AUC is calculated by integrating this function wrt FPF
   tmp <- 1 / lambdaP * log(1 - FPF) + 1
   tmp[tmp < 0] <- pnorm(-20)
   zeta <- qnorm(tmp)
-  TPF <- yROC(zeta, mu, lambdaP, nuP, lesionDistribution)
+  TPF <- RSM_yROC(zeta, mu, lambdaP, nuP, lesDistr) 
   return (TPF)
 }
 
