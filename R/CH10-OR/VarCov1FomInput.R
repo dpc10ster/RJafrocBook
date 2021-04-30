8fa8f590c4c86538fa6100e2659c195f71f18513
dpc10ster <dpc10ster@gmail.com>
2021-04-29 20:51:12 -0400

    weird problem on iMac: filename upper case lower case not updating
    github did not update file
    10-a-or-analysis-introduction.Rmd
    kept old iMac file name as
    10-A-or-analysis-introduction.Rmd
    even though content was updated
    As a result same problem as on commit 3b6227b
    
    with case difference

--- R/CH10-OR/VarCov1FomInput.R
+++ R/CH10-OR/VarCov1FomInput.R
@@ -34,3 +34,42 @@ VarCov1_FomInput <- function (Foms)
     Cov1 = Cov1
   ))  
 }
+
+
+
+VarCov2_FomInput <- function (Foms) 
+{ 
+  J <- dim(Foms)[1]
+  
+  Covariance <- array(dim = c(J, J))
+  for (j in 1:J){
+    for (jp in 1:J){
+      Covariance[j, jp] <- cov(Foms[j, ], Foms[jp, ])          
+    }
+  }
+  
+  Var <- 0
+  count <- 0
+  for (j in 1:J){    
+    Var <- Var + Covariance[j, j] 
+    count <- count + 1
+  }
+  Var <- Var / count 
+  
+  Cov2 <- 0
+  count <- 0
+  for (j in 1:J){    
+    for (jp in 1:J){
+      if (jp != j){
+        Cov2 <- Cov2 + Covariance[j, jp] 
+        count <- count + 1
+      }
+    }
+  }  
+  Cov2 <- Cov2 / count 
+  
+  return (list (    
+    Var = Var,
+    Cov2 = Cov2
+  ))  
+}
